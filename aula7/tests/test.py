89239fcc0b6e6d54793dbe84ab5462038b508935
lucasltinoco <lucasltinoco@gmail.com>
2026-05-12 10:03:55 -0300

    tests: Adicionar Funcionario

--- aula7/tests/test.py
+++ aula7/tests/test.py
@@ -3,12 +3,15 @@ from Empresa import Empresa
 
 class Test(unittest.TestCase):
     def setUp(self):
-      pass
-    
-    def test_cria_empresa_W(self):
-      result = Empresa("W")
-      self.assertIsInstance(result, Empresa)
+        pass
 
+    def test_cria_empresa_W(self):
+        result = Empresa("W")
+        self.assertIsInstance(result, Empresa)
+
+    def test_cria_funcionario_joao(self):
+        funcionario = Funcionario("Joao")
+        self.assertIsInstance(funcionario, Funcionario)
 
 if __name__ == "__main__":
     unittest.main()
