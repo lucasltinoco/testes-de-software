03830e24462a852a693c1feb3d6ab583cee9a155
lucasltinoco <lucasltinoco@gmail.com>
2026-05-12 10:32:38 -0300

    tests: Inserir Projeto na Empresa

--- aula7/tests/test.py
+++ aula7/tests/test.py
@@ -24,6 +24,12 @@ class Test(unittest.TestCase):
     def test_cria_Projeto(self):
         projeto = Project("1")
         self.assertIsInstance(projeto, Project)
+        
+    def test_incluir_projeto_na_empresa(self):
+        EmpresaW = Enterprise("W")
+        projeto2 = Projeto("2")
+        EmpresaW.insertProject(projeto2)
+        self.assertIn(projeto2, EmpresaW.Projects)
 
 if __name__ == "__main__":
     unittest.main()
