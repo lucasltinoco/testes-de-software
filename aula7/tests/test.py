d28c375a7612d339a877e5c5c1776228ebe462fb
Ruan Alboni Ferreira <117960353+RuanAlboni@users.noreply.github.com>
2026-05-24 06:12:10 -0300

    refac: adiciona path de src para execução

--- aula7/tests/test.py
+++ aula7/tests/test.py
@@ -1,3 +1,11 @@
+from pathlib import Path
+import sys
+
+ROOT_DIR = Path(__file__).resolve().parents[1]
+SRC_DIR = ROOT_DIR / "src"
+
+sys.path.insert(0, str(SRC_DIR))
+
 import unittest
 from TestHelper import TestHelper
 from Enterprise import Enterprise
